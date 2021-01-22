8baf75fd8761a0d10585134b531906744c1eda29
Rishabh Kohale <r.kohale9@gmail.com>
2021-01-22 16:14:13 +0530

    subject chapter and topic migration added

--- modules/question/server-ts/migrations/008_question.js
+++ modules/question/server-ts/migrations/008_question.js
@@ -1,5 +1,44 @@
 exports.up = function(knex) {
   return Promise.all([
+    knex.schema.createTable('subject', (table) => {
+      table.increments();
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('user')
+        .onDelete('CASCADE');
+      table.string('title');
+      table.string('description');
+      table.boolean('is_active').defaultTo(true);
+      table.timestamps(false, true);
+    }),
+    knex.schema.createTable('chapter', (table) => {
+      table.increments();
+      table
+        .integer('subject_id')
+        .unsigned()
+        .references('id')
+        .inTable('subject')
+        .onDelete('CASCADE');
+      table.string('title');
+      table.string('description');
+      table.boolean('is_active').defaultTo(true);
+      table.timestamps(false, true);
+    }),
+    knex.schema.createTable('topic', (table) => {
+      table.increments();
+      table
+        .integer('chapter_id')
+        .unsigned()
+        .references('id')
+        .inTable('chapter')
+        .onDelete('CASCADE');
+      table.string('title');
+      table.string('description');
+      table.boolean('is_active').defaultTo(true);
+      table.timestamps(false, true);
+    }),
     // knex.schema.createTable("quiz", (table) => {
     //   table.increments();
     //   table
@@ -30,30 +69,31 @@ exports.up = function(knex) {
     //   table.boolean("is_active").defaultTo(true);
     //   table.timestamps(false, true);
     // }),
-    knex.schema.createTable("question", (table) => {
-      table.increments();
-      //   table
-      //     .integer("section_id")
-      //     .unsigned()
-      //     .references("id")
-      //     .inTable("section")
-      //     .onDelete("CASCADE");
-      table.string("description", 5000);
-      //   table.integer("dependent_question_id");
-      //   table.integer("dependent_choice_id");
-      table.string("question_type");
-      table.boolean("is_active").defaultTo(true);
-      table.timestamps(false, true);
-    }),
-    knex.schema.createTable("choice", (table) => {
+    knex.schema.createTable('question', (table) => {
       table.increments();
       table
-        .integer("question_id")
+        .integer('topic_id')
         .unsigned()
-        .references("id")
-        .inTable("question")
-        .onDelete("CASCADE");
-      table.string("description", 5000);
+        .references('id')
+        .inTable('topic')
+        .onDelete('CASCADE');
+      table.string('description', 5000);
+      //   table.integer("dependent_question_id");
+      //   table.integer("dependent_choice_id");
+      table.string('question_type');
+      table.boolean('is_active').defaultTo(true);
+      table.timestamps(false, true);
+    }),
+    knex.schema.createTable('choice', (table) => {
+      table.increments();
+      table
+        .integer('question_id')
+        .unsigned()
+        .references('id')
+        .inTable('question')
+        .onDelete('CASCADE');
+      table.string('description', 5000);
+      table.boolean('is_active').defaultTo(true);
       table.timestamps(false, true);
     }),
     //     knex.schema.createTable("attempt", (table) => {
@@ -102,8 +142,8 @@ exports.down = function(knex) {
   return Promise.all([
     // knex.schema.dropTable("answer"),
     // knex.schema.dropTable("attempt"),
-    knex.schema.dropTable("choice"),
-    knex.schema.dropTable("question"),
+    knex.schema.dropTable('choice'),
+    knex.schema.dropTable('question'),
     // knex.schema.dropTable("section"),
     // knex.schema.dropTable("quiz"),
   ]);
