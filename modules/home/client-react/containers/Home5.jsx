bd0714a76ddb9583ae0eb5d54777728b602116b8
Rishabh Kohale <r.kohale9@gmail.com>
2021-03-19 21:44:24 +0530

    addtocart bug fixed

--- modules/home/client-react/containers/Home5.jsx
+++ modules/home/client-react/containers/Home5.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { translate } from '@gqlapp/i18n-client-react';
+import { compose } from '@gqlapp/core-common';
+import { withCurrentUser } from '@gqlapp/user-client-react/containers/UserOperations';
+
 import HomeView5 from '../components/HomeView5';
 
 const Home5 = props => {
   return <HomeView5 {...props} />;
 };
 
-export default translate('home')(Home5);
+export default compose(withCurrentUser, translate('home'))(Home5);
